410b0a14dad73485894b6742e4cd826c5512311e
ManuelGeorges <manuel.georges2009@email.com>
2025-09-21 14:54:37 +0300

    patch 1.1.1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['arabic-stemmer'],
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/search',
-        destination: 'http://localhost:5000/api/search',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
