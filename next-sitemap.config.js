588d187df756b39c05ff6c970e0bb6556e1a92ea
ManuelGeorges <manuel.georges2009@email.com>
2025-09-24 23:06:53 +0300

    modified rules for sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-/ ** @type {import('next-sitemap').IConfig} */
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://example.com",
+  siteUrl: "https://agios-bible.vercel.app/",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
