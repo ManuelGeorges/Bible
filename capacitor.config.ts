18de0bc59e55976b514b8ad3a07088daef5d64d3
ManuelGeorges <manuel.georges2009@gmail.com>
2026-03-19 16:41:20 +0200

    YES!!

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.agios.bible',
   appName: 'Agios Bible',
   webDir: 'out',
-  "server": {
-    "url": "https://agios-bible.vercel.app/",
-    "cleartext": true
-  },
 
 };
 export default config;
