dc12743eb1175026282ecabe2b3a382bbddaba5c
ManuelGeorges <manuel.georges2009@gmail.com>
2026-07-11 21:05:22 +0300

    2.1.2 resolved

--- ios/add_widgets.rb
+++ ios/add_widgets.rb
@@ -1,42 +1,45 @@
 require 'xcodeproj'
 
-# المسار إلى ملف المشروع (يتم تشغيل السكريبت من مجلد ios)
-project_path = 'App/App.xcodeproj'
+# 1. إعداد المسارات
+project_path = 'AgiosBible.xcodeproj'
+widget_name = 'AgiosWidget'
+widget_bundle_id = 'com.agios.bible.widget'
+app_group_id = 'group.com.agios.bible'
+app_entitlements = 'AgiosBible/AgiosBible.entitlements'
+widget_entitlements = 'AgiosWidget/AgiosWidget.entitlements'
+
 project = Xcodeproj::Project.open(project_path)
 
-# 1. تحديد الأهداف
-main_target = project.targets.find { |t| t.name == 'App' }
-group_name = 'AgiosWidgets'
+# 2. التأكد من وجود الـ App Group في التطبيق الأساسي
+main_target = project.targets.find { |t| t.name == 'AgiosBible' }
+raise "Main target not found" unless main_target
 
-# 2. إنشاء مجموعة للملفات (Group)
-group = project.main_group.find_subpath(group_name, true)
+# 3. إعداد الـ Widget Target
+# (نفترض أن الـ Widget مضاف مسبقاً أو يتم إنشاؤه هنا)
+widget_target = project.targets.find { |t| t.name == widget_name }
+unless widget_target
+  widget_target = project.new_target(:app_extension, widget_name, :ios, '14.0')
+  widget_target.product_type = 'com.apple.product-type.app-extension'
+  widget_target.build_configurations.each do |config|
+    config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = widget_bundle_id
+    config.build_settings['INFOPLIST_FILE'] = "#{widget_name}/Info.plist"
+    config.build_settings['CODE_SIGN_ENTITLEMENTS'] = widget_entitlements
+    config.build_settings['DEVELOPMENT_TEAM'] = 'XMBVV283C4'
+  end
+end
 
-# 3. تحديد المسارات النسبية للملفات
-swift_file = 'App/AgiosWidgets.swift'
-plist_file = 'App/AgiosWidgets.plist'
-widget_entitlements = 'App/AgiosWidgets.entitlements'
-app_entitlements = 'App/App/App.entitlements'
+# 4. إضافة الملفات للويدجت
+widget_folder = project.main_group.find_subpath(widget_name, true)
+swift_file = "#{widget_name}/#{widget_name}.swift"
+swift_ref = widget_folder.new_file(swift_file)
 
-# 4. إضافة مراجع الملفات للمشروع
-swift_ref = group.new_reference(swift_file)
-plist_ref = group.new_reference(plist_file)
-entitlements_ref = group.new_reference(widget_entitlements)
-
-# 5. إنشاء الـ Widget Target (Extension)
-old_target = project.targets.find { |t| t.name == group_name }
-old_target.remove_from_project if old_target
-
-widget_target = project.new_target(:app_extension, group_name, :ios, '14.0')
-
-# 6. ضبط إعدادات البناء والتوقيع للويدجت
+# 5. تفعيل الـ App Groups للويدجت
 widget_target.build_configurations.each do |config|
-  config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = "com.agios.bible.AgiosWidgets"
-  config.build_settings['INFOPLIST_FILE'] = plist_file
   config.build_settings['CODE_SIGN_ENTITLEMENTS'] = widget_entitlements
-  config.build_settings['SWIFT_VERSION'] = '5.0'
-  config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '14.0'
-  config.build_settings['DEVELOPMENT_TEAM'] = 'XMBVV283C4'
-  config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
+end
+
+# 6. تحديث إعدادات الـ Build للويدجت
+widget_target.build_configurations.each do |config|
   config.build_settings['SKIP_INSTALL'] = 'YES'
 end
 
@@ -53,9 +56,9 @@ widget_target.source_build_phase.add_file_reference(swift_ref)
 main_target.add_dependency(widget_target)
 
 # 10. إضافة مرحلة Embed App Extensions
-embed_phase = main_target.copy_files_build_phases.find { |p| p.symbol_purpose == :plugins } ||
+embed_phase = main_target.copy_files_build_phases.find { |p| p.dst_subfolder_spec == '13' } ||
               main_target.new_copy_files_build_phase('Embed App Extensions')
-embed_phase.symbol_purpose = :plugins
+embed_phase.dst_subfolder_spec = '13'
 embed_phase.add_file_reference(widget_target.product_reference)
 
 project.save
