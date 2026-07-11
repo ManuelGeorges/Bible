1d3dc344517559dc47f7026a2a51b1ad0b08feeb
ManuelGeorges <manuel.georges2009@gmail.com>
2026-07-11 21:15:59 +0300

    2.1.2 swift version 5.0

--- ios/add_widgets.rb
+++ ios/add_widgets.rb
@@ -1,21 +1,20 @@
 require 'xcodeproj'
 
-# 1. إعداد المسارات (تعديل لتناسب Capacitor)
+# 1. إعداد المسارات
 project_path = 'App/App.xcodeproj'
 main_target_name = 'App'
 widget_name = 'AgiosWidget'
 widget_bundle_id = 'com.agios.bible.widget'
-app_group_id = 'group.com.agios.bible'
 
-# المسارات بالنسبة لمجلد ios
-app_entitlements = 'App/App/App.entitlements'
-widget_entitlements = 'App/AgiosWidgets.entitlements'
-swift_file = 'App/AgiosWidgets.swift'
+# المسارات بالنسبة لمجلد App (حيث يوجد الملف .xcodeproj)
+app_entitlements_path = 'App/App.entitlements'
+widget_entitlements_path = 'AgiosWidgets.entitlements'
+widget_plist_path = 'AgiosWidgets.plist'
 
 puts "Opening project at #{project_path}..."
 project = Xcodeproj::Project.open(project_path)
 
-# 2. التأكد من وجود الـ Target الرئيسي
+# 2. البحث عن الـ Target الرئيسي
 main_target = project.targets.find { |t| t.name == main_target_name }
 raise "Main target '#{main_target_name}' not found" unless main_target
 
@@ -24,43 +23,49 @@ widget_target = project.targets.find { |t| t.name == widget_name }
 unless widget_target
   puts "Creating new widget target: #{widget_name}..."
   widget_target = project.new_target(:app_extension, widget_name, :ios, '14.0')
-  widget_target.product_type = 'com.apple.product-type.app-extension'
-
-  widget_target.build_configurations.each do |config|
-    config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = widget_bundle_id
-    config.build_settings['CODE_SIGN_ENTITLEMENTS'] = widget_entitlements
-    config.build_settings['DEVELOPMENT_TEAM'] = 'XMBVV283C4' # تأكد من أن هذا الـ Team ID صحيح
-    config.build_settings['SKIP_INSTALL'] = 'YES'
-  end
 end
 
-# 4. إضافة ملف الكود للويدجت
-# بنضيف الملف لمجلد المشروع في Xcode ونربطه بالـ Target
-widget_group = project.main_group.find_subpath('App', true)
-swift_ref = widget_group.find_file_by_path(File.basename(swift_file)) || widget_group.new_file(File.basename(swift_file))
+# ضبط الإعدادات لكل الـ Configurations (Debug & Release)
+widget_target.build_configurations.each do |config|
+  config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = widget_bundle_id
+  config.build_settings['PRODUCT_NAME'] = widget_name
+  config.build_settings['SWIFT_VERSION'] = '5.0'
+  config.build_settings['SKIP_INSTALL'] = 'YES'
+  config.build_settings['CODE_SIGN_ENTITLEMENTS'] = widget_entitlements_path
+  config.build_settings['INFOPLIST_FILE'] = widget_plist_path
+  config.build_settings['DEVELOPMENT_TEAM'] = 'XMBVV283C4'
+  config.build_settings['LD_RUNPATH_SEARCH_PATHS'] = '$(inherited) @executable_path/Frameworks @executable_path/../../Frameworks'
+end
 
-# 8. إضافة ملف الكود لمرحلة Compile الخاصة بالويدجت
+# 4. إضافة الملفات للـ Xcode Project Groups
+# بنضيفهم لـ Group اسمه "AgiosWidget" عشان التنظيم
+widget_group = project.main_group['AgiosWidget'] || project.main_group.new_group('AgiosWidget')
+
+# إضافة ملف الـ Swift (موجود في ios/App/)
+swift_file_path = 'AgiosWidgets.swift'
+swift_ref = widget_group.find_file_by_path(swift_file_path) || widget_group.new_file(swift_file_path)
+
+# التأكد من إضافة الملف لمرحلة الـ Build
+widget_target.source_build_phase.clear
 widget_target.source_build_phase.add_file_reference(swift_ref)
 
-# 9. ربط الويدجت بالتطبيق كـ Dependency
-unless main_target.dependencies.any? { |d| d.target.name == widget_name }
+# 5. ربط الويدجت بالتطبيق (Dependency & Embed)
+unless main_target.dependencies.any? { |d| d.target && d.target.name == widget_name }
   main_target.add_dependency(widget_target)
 end
 
-# 10. إضافة مرحلة Embed App Extensions
-# في Capacitor، بنحتاج نتأكد إن الويدجت بيتنسخ جوه التطبيق
 embed_phase = main_target.copy_files_build_phases.find { |p| p.dst_subfolder_spec == '13' } ||
               main_target.new_copy_files_build_phase('Embed App Extensions')
-embed_phase.dst_subfolder_spec = '13' # 13 تعني مجلد Plugins/Extensions
+embed_phase.dst_subfolder_spec = '13'
 
 unless embed_phase.files_references.any? { |f| f.path == widget_target.product_reference.path }
   embed_phase.add_file_reference(widget_target.product_reference)
 end
 
-# تحديث إعدادات الـ Entitlements للـ Target الرئيسي
+# 6. تحديث إعدادات التطبيق الرئيسي
 main_target.build_configurations.each do |config|
-  config.build_settings['CODE_SIGN_ENTITLEMENTS'] = app_entitlements
+  config.build_settings['CODE_SIGN_ENTITLEMENTS'] = app_entitlements_path
 end
 
 project.save
-puts "Successfully configured iOS Project with Widgets and App Groups!"
+puts "Successfully configured iOS Project with Widgets!"
