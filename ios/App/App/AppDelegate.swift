d02b1eb662e28c49d3de55520bab45a057a30723
ManuelGeorges <manuel.georges2009@gmail.com>
2026-08-03 13:45:57 -0700

    2.4.0 ios

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
     }
 
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @swift.UNNotificationPresentationOptions) -> Void {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .sound, .badge])
     }
 
